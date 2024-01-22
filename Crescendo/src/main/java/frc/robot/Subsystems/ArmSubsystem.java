54fd487dd0b7b0ec82b1a9e2debab1817e280c3a
Andrés Monsivais Salazar <92643505+parlack@users.noreply.github.com>
2024-01-22 16:24:18 -0600

    Add template Arm and libraries

---
+++ Crescendo/src/main/java/frc/robot/Subsystems/ArmSubsystem.java
@@ -0,0 +1,17 @@
+package frc.robot.Subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ArmSubsystem extends SubsystemBase {
+
+  public ArmSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  
+
+
+}
