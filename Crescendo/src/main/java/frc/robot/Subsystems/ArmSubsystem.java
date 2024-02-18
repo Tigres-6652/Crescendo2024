ec0d5ea896f295d4f8399b4a69fba1f6f2b70a10
PapuMcLovin <jesus.garciardrg@uanl.edu.mx>
2024-02-18 16:15:04 -0600

    1.0
    codigo pal brazo

--- Crescendo/src/main/java/frc/robot/Subsystems/ArmSubsystem.java
+++ Crescendo/src/main/java/frc/robot/Subsystems/ArmSubsystem.java
@@ -1,6 +1,11 @@
 package frc.robot.Subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -8,6 +13,29 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase {
 
+
+  CANSparkMax RgtMtrArm = new CANSparkMax(7, MotorType.kBrushless);
+  CANSparkMax LftMtrArm = new CANSparkMax(8, MotorType.kBrushless);
+
+  ProfiledPIDController PIDRgtMtrArm = new ProfiledPIDController(0.05, 0.0005, 0.00, new TrapezoidProfile.Constraints(80,90));
+  ProfiledPIDController PIDLftMtrArm = new ProfiledPIDController(0.05, 0.0005, 0.00, new TrapezoidProfile.Constraints(80,90));
+
+  public void RgtPstnVrbl (double Grado1) {
+    var signal= RgtMtrArm.getEncoder();
+    double RgtNeoEnc = signal.getPosition();
+    RgtMtrArm.set(PIDRgtMtrArm.calculate(RgtNeoEnc, Grado1));
+    SmartDashboard.putNumber("Posicion Abajo", RgtNeoEnc);
+  }
+
+  public void LftPstnVrbl (double Grado2) {
+    var signal= LftMtrArm.getEncoder();
+    double LftNeoEnc = signal.getPosition();
+    LftMtrArm.set(PIDLftMtrArm.calculate(LftNeoEnc, Grado2));
+    SmartDashboard.putNumber("Posicion Abajo", LftNeoEnc);
+  }
+
+
+/*
   TalonFX MAmr1 = new TalonFX (7);
   
   ProfiledPIDController pidController =new ProfiledPIDController
@@ -18,26 +46,28 @@ public class ArmSubsystem extends SubsystemBase {
     double FxEnc1 = signal.getValue();
     MAmr1.set(pidController.calculate(FxEnc1, Grado1));
     SmartDashboard.putNumber("Posicion Abajo", FxEnc1);
+  } 
+*/
+
+  public void stop (double speed){
+    RgtMtrArm.set(0);
   }
 
   public void Botones (boolean Boton_0, boolean Boton_90){
 
     if((Boton_0)){
-      PosicionVariable(0);
-    }else if (Boton_90){
-      PosicionVariable(90);
-    }else{
-      stop();
-    }
-  }
+      RgtPstnVrbl(0);
 
-  public void stop(){
-    MAmr1.set(0);
+    }else if (Boton_90){
+      RgtPstnVrbl(90);
+
+    }else{
+      stop(0);
+    }
   }
 
   public ArmSubsystem() {}
 
   @Override
   public void periodic() {}
-
 }
