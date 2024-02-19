4d85aaca882b0e5e773fb07eaee9bc7ab8bb06bd
PapuMcLovin <jesus.garciardrg@uanl.edu.mx>
2024-02-18 18:59:10 -0600

    2.0
    extras

--- Crescendo/src/main/java/frc/robot/Subsystems/ArmSubsystem.java
+++ Crescendo/src/main/java/frc/robot/Subsystems/ArmSubsystem.java
@@ -1,39 +1,151 @@
 package frc.robot.Subsystems;
 
-import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase {
-
-
+//declaracion de los motores Neo
   CANSparkMax RgtMtrArm = new CANSparkMax(7, MotorType.kBrushless);
   CANSparkMax LftMtrArm = new CANSparkMax(8, MotorType.kBrushless);
 
+//Declaracion de los motores
+  DigitalInput FrwrdLmt = new DigitalInput(8);
+  DigitalInput BckLmt = new DigitalInput(9);
+
+//Control del PID
   ProfiledPIDController PIDRgtMtrArm = new ProfiledPIDController(0.05, 0.0005, 0.00, new TrapezoidProfile.Constraints(80,90));
   ProfiledPIDController PIDLftMtrArm = new ProfiledPIDController(0.05, 0.0005, 0.00, new TrapezoidProfile.Constraints(80,90));
 
-  public void RgtPstnVrbl (double Grado1) {
+//Numerode Vueltas Motor derecho
+  public void RgtPstnVrbl (double Vueltas1) {
     var signal= RgtMtrArm.getEncoder();
     double RgtNeoEnc = signal.getPosition();
-    RgtMtrArm.set(PIDRgtMtrArm.calculate(RgtNeoEnc, Grado1));
+    RgtMtrArm.set(PIDRgtMtrArm.calculate(RgtNeoEnc, Vueltas1));
     SmartDashboard.putNumber("Posicion Abajo", RgtNeoEnc);
   }
 
-  public void LftPstnVrbl (double Grado2) {
+//Numero de vueltas Motor Izquierdo
+  public void LftPstnVrbl (double Vueltas2) {
     var signal= LftMtrArm.getEncoder();
     double LftNeoEnc = signal.getPosition();
-    LftMtrArm.set(PIDLftMtrArm.calculate(LftNeoEnc, Grado2));
+    LftMtrArm.set(PIDLftMtrArm.calculate(LftNeoEnc, Vueltas2));
     SmartDashboard.putNumber("Posicion Abajo", LftNeoEnc);
   }
 
+//Metodo para invertir y seguimiente de los motores
+  public void MtrArmInvrtAndFllw (){
+    RgtMtrArm.setInverted(false);
+    LftMtrArm.setInverted(true);
+
+    RgtMtrArm.follow(LftMtrArm);
+    LftMtrArm.follow(RgtMtrArm);
+  }
+
+//Convercion de vueltas a grados
+  public double Axis_Degrees (){
+    double PulSnsr = RgtMtrArm.getEncoder().getPosition();
+    double ShftTrnRdctn = ((PulSnsr) / (100));
+    double AxisDgrs = (ShftTrnRdctn * 0.006975 * (360)) ;
+    return AxisDgrs;
+
+  }
+
+//Bloqueo de Los motores
+  public void stop (double speed){
+    RgtMtrArm.set(0);
+
+  }
+
+//Limite y moviento libre de los motores
+  public void LmtMtr (Double velsup){
+    double x = 0;
+    double y = 0;
+
+    double a = 0;
+    double b = 0;
+    double gradosmaximos = 0;
+    double limiteposterior = 0;
+
+      if (BckLmt.get() && Axis_Degrees() > -y) {
+        RgtMtrArm.set(-velsup);
+        LftMtrArm.set(-velsup);
+
+      } else if (!BckLmt.get() && velsup < 0.001) {
+        RgtMtrArm.set(-velsup);
+        LftMtrArm.set(-velsup);
+  
+      } else if (!BckLmt.get() && velsup > 0.001) {
+        RgtMtrArm.set(0);
+        LftMtrArm.set(0);
+  
+      } else if (Axis_Degrees() < -x && velsup > 0.001 && BckLmt.get()) {
+        RgtMtrArm.set(-velsup);
+        LftMtrArm.set(-velsup);
+
+      } else if (Axis_Degrees() < -x && velsup < 0.001 && BckLmt.get()) {
+        RgtMtrArm.set(0);
+        LftMtrArm.set(0);
+  
+      }  
+
+      if (FrwrdLmt.get() && Axis_Degrees() > b) {
+        RgtMtrArm.set(-velsup);
+        LftMtrArm.set(-velsup);
+  
+      } else if (!FrwrdLmt.get() && velsup < gradosmaximos) {
+        RgtMtrArm.set(-velsup);
+        LftMtrArm.set(-velsup);
+
+      } else if (!FrwrdLmt.get() && velsup > gradosmaximos) {
+        RgtMtrArm.set(0);
+        LftMtrArm.set(0);
+  
+      } else if (Axis_Degrees() < a && velsup > gradosmaximos && FrwrdLmt.get()) {
+        RgtMtrArm.set(-velsup);
+        LftMtrArm.set(-velsup);
+
+      } else if (Axis_Degrees() < a && velsup < gradosmaximos && FrwrdLmt.get()) {
+        RgtMtrArm.set(0);
+        LftMtrArm.set(0);
+  
+      }  
+
+      if (!FrwrdLmt.get()) {
+        RgtMtrArm.getEncoder().setPosition(limiteposterior);
+        LftMtrArm.getEncoder().setPosition(limiteposterior);
+      }
+
+      if (!BckLmt.get()) {
+        RgtMtrArm.getEncoder().setPosition(0);
+        LftMtrArm.getEncoder().setPosition(0);
+      }
+      
+    }
+
+//Controles para pruebas
+  public void Botones (boolean Boton_0, boolean Boton_90){
+    if((Boton_0)){
+      RgtPstnVrbl(0);
+
+    }else if (Boton_90){
+      RgtPstnVrbl(90);
+
+    }else{
+      stop(0);
+    }
+
+  }
+
+  public ArmSubsystem() {}
+
+  @Override
+  public void periodic() {}
 
 /*
   TalonFX MAmr1 = new TalonFX (7);
@@ -49,25 +161,27 @@ public class ArmSubsystem extends SubsystemBase {
   } 
 */
 
-  public void stop (double speed){
-    RgtMtrArm.set(0);
-  }
+/*Calculos para la reduccion
+*
+ * Reducciones 
+ * 50
+ * 48
+ * 
+ * 1.041666
+ * 
+ * 64
+ * 15
+ * 0.234675
+ * 
+ * 1:5
+ * 1:7
+ * 
+ * 1:35
+ * 0.028571
+ * 
+ * total
+ * 0.006975
+ * 
+ */
 
-  public void Botones (boolean Boton_0, boolean Boton_90){
-
-    if((Boton_0)){
-      RgtPstnVrbl(0);
-
-    }else if (Boton_90){
-      RgtPstnVrbl(90);
-
-    }else{
-      stop(0);
-    }
-  }
-
-  public ArmSubsystem() {}
-
-  @Override
-  public void periodic() {}
 }
